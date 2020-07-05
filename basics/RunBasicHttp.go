c51d0f4b00c678bbd9fb1e6bae2cc2043d24b34f
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-07-05 22:38:15 +0530

    program to run a basic http server

--- basics/RunBasicHttp.go
+++ basics/RunBasicHttp.go
@@ -1,6 +1,15 @@
 package codinggoweb
 
+import (
+	"fmt"
+	"net/http"
+)
+
 // RunBasicHTTP : function to run a basic http server
 func RunBasicHTTP() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you have requested: %s\n", r.URL.Path)
+	})
 
+	http.ListenAndServe(":8080", nil)
 }
